298208500b739555e71da5c2949178efe2e6ae1b
saidulixlam <saidulixlam@gmail.com>
2023-07-28 11:55:50 +0530

    Integrating REST api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const forms = document.querySelector('#form');
-const main = document.getElementById('container2');//cntainer
-const list = document.createElement('ul');
+const list = document.getElementById('list');
+const submit = document.getElementById('submit')
 list.className = 'list-items';
 
 const expense = document.querySelector('#expense');
@@ -19,17 +19,30 @@ function formInput(e) {
         description,
         category
     }
-    localStorage.setItem(obj.description, JSON.stringify(obj));
-    showFun(obj);//tasks to be done function
-    expense.value = '';
-    expdescription.value = '';
-    expcategory.value = '';
+    axios.post("https://crudcrud.com/api/e3914045dfd3417c8597ffa68a44fb53/expenses", obj)
+        .then((res) => {
+            showFun(res.data);
+            clearInput();
+        }).catch((err) => {
+            console.log(err);
+        })
+
 }
+window.addEventListener('DOMContentLoaded', () => {
+    axios.get("https://crudcrud.com/api/e3914045dfd3417c8597ffa68a44fb53/expenses")
+        .then((res) => {
+            for (var i = 0; i < res.data.length; i++) {
+                showFun(res.data[i]);
+            }
+        }).catch((err) => {
+            console.log(err);
+        })
+})
 function showFun(obj) {
     const li = document.createElement('li');
-    li.className = 'li-item float-right';
-    li.appendChild(document.createTextNode(obj.amount + " - " + obj.description + " - " + obj.category));
+    li.className = 'li-item float-right my-3 ';
 
+    li.appendChild(document.createTextNode(obj.amount + " - " + obj.description + " - " + obj.category));
     //delete btn
     const dltbtn = document.createElement('button');
     dltbtn.className = 'btn btn-danger mx-2 float-right delete';
@@ -43,19 +56,35 @@ function showFun(obj) {
     li.appendChild(edtbtn);
 
     list.appendChild(li);
-    main.appendChild(list);
 
-     dltbtn.addEventListener('click', funRemove);
-     function funRemove(){
+    //main.appendChild(list);
+
+    dltbtn.addEventListener('click', funRemove);
+    function funRemove() {
+        axios.delete(`https://crudcrud.com/api/e3914045dfd3417c8597ffa68a44fb53/expenses/${obj._id}`)
+            .then((res) => {
+                console.log(res);
+            }).catch((err) => {
+                console.log(err);
+            })
         list.removeChild(li);
-        localStorage.removeItem(obj.description);
-     }
-  
+    }
+
     edtbtn.addEventListener('click', () => {
+        console.log(submit.value);
+        submit.value = "Update Expense"
         expense.value = obj.amount;
         expdescription.value = obj.description;
         expcategory.value = obj.category;
         funRemove();
+
     });
 
+}
+
+function clearInput() {
+    expense.value = '';
+    expdescription.value = '';
+    expcategory.value = '';
+    submit.value = "Add Expense"
 }
